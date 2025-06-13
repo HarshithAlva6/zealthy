6ceb74c4ed92de8f7bd641034a2e4926a72f165e
Harshith <harshalva.titan@csu.fullerton.edu>
2025-06-12 22:08:44 -0700

    Zealthy Onboarding app changes

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// .eslintrc.js
+module.exports = {
+    // ...existing config
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  };
+  
