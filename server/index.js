404457b1242d5ed8ad5524d749e71ebc1fbc8e53
Harshith <harshalva.titan@csu.fullerton.edu>
2025-06-13 00:14:33 -0700

    Zealthy add credentials for CORS

--- server/index.js
+++ server/index.js
@@ -14,13 +14,7 @@ const allowedOrigins = [
 ];
 // Middleware
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins, 
   credentials: true,              
 }));
 app.use(express.json());
